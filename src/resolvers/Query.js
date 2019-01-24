1495671bc08d0cd9019dbb37449759c552be3b8e
Marc Immediato <marc.immediato@gmail.com>
2019-01-23 20:21:38 -0500

    moving resolvers and typedefs to separate files

---
+++ src/resolvers/Query.js
@@ -0,0 +1,16 @@
+const Query = {
+  users() {
+    return [
+      {
+        id: '1',
+        name: 'Marc'
+      },
+      {
+        id: '2',
+        name: 'John'
+      }
+    ];
+  }
+};
+
+export { Query as default };

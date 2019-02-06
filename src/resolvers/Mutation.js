4f2f41953e8c3761d756d93498ffa07612637021
Marc Immediato <marc.immediato@gmail.com>
2019-02-06 18:06:48 -0500

    adding password hashing

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,12 +1,25 @@
+import bcrypt from 'bcryptjs';
+
 const Mutation = {
   async createUser(parent, args, { prisma }, info) {
     const userExists = await prisma.exists.User({ email: args.data.email });
 
+    if (args.data.password.length < 8) {
+      throw new Error('Password must be 8 characters or longer.')
+    }
+    
     if (userExists) {
       throw new Error('Email exists');
     }
 
-    return await prisma.mutation.createUser({ data: args.data }, info);
+    const password = await bcrypt.hash(args.data.password, 10)
+
+    return await prisma.mutation.createUser({ 
+      data: {
+        ...args.data, 
+        password
+      } 
+    }, info);
   }
 };
 
